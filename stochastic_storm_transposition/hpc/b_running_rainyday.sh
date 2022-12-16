e0386bad0a465ff8b17c31513eadf4dd626d9c4d
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2022-12-16 11:44:28 -0500

    made runnable on Rivanna using __directories.sh

--- stochastic_storm_transposition/hpc/b_running_rainyday.sh
+++ stochastic_storm_transposition/hpc/b_running_rainyday.sh
@@ -1,30 +1,23 @@
-ijob -c 1 -A quinnlab_paid -p standard --time=1-00:00:00 --mem=175000
+#!/bin/bash
+#SBATCH -o _script_outputs/%x/%A_%a_%N.out
+#SBATCH -e _script_errors/%x/%A_%a_%N.out
+#SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
+#SBATCH -p standard				# Queue name "standard" (serial)
+#SBATCH -A quinnlab_paid				# allocation name
+#SBATCH -t 48:00:00				# Run time per serial job (hh:mm:ss)
+#SBATCH --array=1
+#SBATCH --mem-per-cpu=175000
+#SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
+#SBATCH --mail-type=ALL   
+# SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+
+module purge
 module load anaconda
 source activate rainyday
-cd /project/quinnlab/dcl3nd/norfolk/stormy/stochastic_storm_transposition
+
+source __directories.sh
+
 #%% running on Rivanna
 # using a subset of mrms data
-python "RainyDay2/Source/RainyDay_Py3.py" "norfolk/norfolk_mrms_subset_rivanna.sst"
-# using a subset of hourly data
-python "RainyDay2/Source/RainyDay_Py3.py" "norfolk/norfolk_mrms_hourly_subset_rivanna.sst"
+python ${assar_dirs[hpc_b_py]} ${assar_dirs[hpc_b_SST_in]}
 
-
-
-#%% running on Rivanna (old)
-# using subset of mrms data
-# python "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/norfolk_mrms_subset_rivanna.sst"
-# # using all mrms data
-# python "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/norfolk_mrms_rivanna.sst"
-# # using stage IV data
-# python "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/norfolk_stage_iv_rivanna.sst"
-# # reproducing BigThompson exacmple
-# python "/project/quinnlab/dcl3nd/norfolk/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "/project/quinnlab/dcl3nd/norfolk/RainyDay2/_reproducing_BigThompsonExample/BigThompson72hr_example_rivanna.sst"
-
-# #%% Running on local computer (old)
-# #laptop environment: RainyDay
-# # running using StaveIV data
-# python "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/norfolk_mrms/norfolk_stage_iv_local.sst"
-# # running using subset of mrms data that is also consolidated to an hourly timestep
-# python "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/norfolk_mrms/norfolk_mrms_subset_hourly_local.sst"
-# # reproducing Big Thompson Example
-# python "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/norfolk_mrms/Source/RainyDay_Py3.py" "D:/mrms_processing/scripts_rivanna_revised/RainyDay2/_reproducing_BigThompsonExample/BigThompson72hr_example_local.sst"
