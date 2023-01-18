ebbbb4c42252cada7daca612a8b4a585851188c5
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-01-18 09:36:12 -0500

    setting up to run RainyDay with StageIV data for 2003 to compare with MRMS run

--- stochastic_storm_transposition/hpc/__directories.sh
+++ stochastic_storm_transposition/hpc/__directories.sh
@@ -23,5 +23,8 @@ assar_dirs[hpc_b_py]=${assar_dirs[sst]}"RainyDay2/Source/RainyDay_Py3.py"
 assar_dirs[hpc_b_SST_in_subset]=${assar_dirs[sst]}"norfolk/norfolk_mrms_subset_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_subset]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_subset_short_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_subset2003]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_subset_short_2003_rivanna.sst"
+assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV]=${assar_dirs[sst]}"norfolk/norfolk_stageIV_hourly_subset_short_2003_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_2001_to_2011]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_2001-2011_rivanna.sst"
+
+
 
