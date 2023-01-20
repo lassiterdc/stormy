cf407e3552c81e1fff7d22c8d6f48b67cbcafc34
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-01-20 15:11:49 -0500

    testing putting nas in stage IV data

--- stochastic_storm_transposition/hpc/__directories.sh
+++ stochastic_storm_transposition/hpc/__directories.sh
@@ -24,6 +24,7 @@ assar_dirs[hpc_b_SST_in_subset]=${assar_dirs[sst]}"norfolk/norfolk_mrms_subset_r
 assar_dirs[hpc_b_SST_in_hrly_subset]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_subset_short_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_subset2003]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_subset_short_2003_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV]=${assar_dirs[sst]}"norfolk/norfolk_stageIV_hourly_subset_short_2003_rivanna.sst"
+assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV_with_nas]=${assar_dirs[sst]}"norfolk/norfolk_stageIV_with_nas_hourly_subset_short_2003_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_2001_to_2011]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_2001-2011_rivanna.sst"
 
 
---
+++ stochastic_storm_transposition/norfolk/_troubleshooting negatives/a_replacing_timesteps_with_na_vals.py
@@ -0,0 +1,21 @@
+"""
+10/20/2003 contains the beginning of the largest storm that appears in norfolk's storm catalog. 
+This script replaces a few timesteps with nan values to see what happens.
+THe storm starts at 08:00.
+"""
+
+#%% import libraries and load directories
+import xarray as xr
+import numpy as np
+fpath = "D:/Dropbox/_GradSchool/_norfolk/stormy/stochastic_storm_transposition/norfolk/_troubleshooting negatives/StageIV_FilledCorr.20031030.03degree_original.nc"
+
+#%% load dataset
+ds = xr.load_dataset(fpath)
+
+#%% make all timesteps from 18:00 to 20:00 nan
+ts = ['2003-10-30T18:00:00.000000000','2003-10-30T19:00:00.000000000', '2003-10-30T20:00:00.000000000']
+
+ds.rainrate.loc[{"time":ts}] = np.nan
+
+
+
