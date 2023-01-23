f3e8db8b453d5c6dd48f9d15023565dafdbbb2e0
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-01-23 12:58:56 -0500

    increasing runtime to 6 days

--- stochastic_storm_transposition/hpc/_test_b_running_rainyday_norfolk_mrms_subset_rivanna.sh
+++ stochastic_storm_transposition/hpc/_test_b_running_rainyday_norfolk_mrms_subset_rivanna.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial)
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 72:00:00				# Run time per serial job (hh:mm:ss)
+#SBATCH -t 144:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --array=1
 #SBATCH --mem-per-cpu=250000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
