185281930cad6336090719bbaa2c08919023c0dc
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-01-23 12:59:15 -0500

    investigating negative values in storm catalog

--- stochastic_storm_transposition/local/a_stormcat_processing.py
+++ stochastic_storm_transposition/local/a_stormcat_processing.py
@@ -10,13 +10,36 @@ from glob import glob
 from netCDF4 import Dataset
 fldr_rainyday_working_dir = a_stormat_processing()
 
-#%% load storm catalogs and replace -9999 with NA
 lst_f_strmcats = glob(fldr_rainyday_working_dir + "*.nc")
+
+
+#%%
 
 for f_strmcat in lst_f_strmcats:
     with xr.open_dataset(f_strmcat) as ds:
-
+        continue
 #%% load with netcdf4 library
-nds = Dataset(f_strmcat)
+f_strmcat_stageVI = lst_f_strmcats[3]
+nds = Dataset(f_strmcat_stageVI)
 nds.dimensions["time"]
 nds.variables['precrate']
+
+#%%
+import numpy as np
+f_strmcat_mrms = lst_f_strmcats[1]
+infile = Dataset(f_strmcat_mrms)
+infile.dimensions["time"]
+infile.variables['precrate']
+
+outtime=np.array(infile.variables['time'][:],dtype='datetime64[m]')
+#%%
+# catmax_subdur[i]=dur_max
+# temptime[i,:]=cattime[i,dur_j:dur_j+int(duration*60./rainprop.timeres)]
+# cattime_subdur=temptime
+# sind=np.argsort(catmax_subdur)
+# cattime=cattime_subdur[sind,:]
+
+nstorms = nds.dimensions["nstorms"].size
+cattime=np.empty((nstorms,int(catduration*60/rainprop.timeres)),dtype='datetime64[m]')
+cattime[:]=np.datetime64(datetime(1900,1,1,0,0,0))
+str(cattime[i,-1])
