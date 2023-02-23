fa4265437c756e7f3ecb935df8c4166c743bab24
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-02-23 08:56:45 -0500

    revising to distribute the task over multiple jobs

--- stochastic_storm_transposition/hpc/c_running_rainyday_mrms_hourly.sh
+++ stochastic_storm_transposition/hpc/c_running_rainyday_mrms_hourly.sh
@@ -4,12 +4,23 @@
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial)
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 72:00:00				# Run time per serial job (hh:mm:ss)
-#SBATCH --array=1
-#SBATCH --mem-per-cpu=250000
+#SBATCH -t 6:00:00				# Run time per serial job (hh:mm:ss)
+#SBATCH --array=1-11,15-22	        # Array of jobs to loop through 22 years (2001-2011, 15-22)
+#SBATCH --mem-per-cpu=36000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
 # SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+
+# to use dev:
+# drop out mem-per-cpu line
+# change partition from standard to dev
+# change time to 1 hour
+# can use the command qlimits to see table of computational limits
+# apparently the number of core hours are calculated based on the 
+# sacct 
+# a node is like a computer
+# use -c instead of ntasks if I want to run multi-processing python scripts 
+# ntasks is used for MPI
 
 module purge
 module load anaconda
@@ -17,7 +28,21 @@ source activate rainyday
 
 source __directories.sh
 
-#%% running on Rivanna
-# using a subset of mrms data
-python ${assar_dirs[hpc_b_py]} ${assar_dirs[hpc_c_SST_mrms_hourly]}
+# test to confirm I can import the shapefiles library
+# echo `python "_c_tbshooting.py"`
 
+# assign year variable
+if [ ${SLURM_ARRAY_TASK_ID} -lt 10 ]
+then
+	year=200${SLURM_ARRAY_TASK_ID}
+else
+	year=20${SLURM_ARRAY_TASK_ID}
+fi
+
+# generate SST script for the year
+sst_in=$(python ${assar_dirs[hpc_c_py]} ${year})
+
+echo "Running sst for year $year using file $sst_in..."
+
+# running RainyDay
+python ${assar_dirs[hpc_rainyday_py]} ${sst_in}
