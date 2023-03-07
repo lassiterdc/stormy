0e5b11de5330656989142c56f0bc1db75f4464ba
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-03-07 16:20:08 -0500

    updated to include stuff for script da

--- stochastic_storm_transposition/hpc/__utils.py
+++ stochastic_storm_transposition/hpc/__utils.py
@@ -4,25 +4,25 @@
 dir_repo = "/project/quinnlab/dcl3nd/norfolk/stormy/"
 dir_sst = dir_repo + "stochastic_storm_transposition/"
 dir_sst_nrflk = dir_sst + "norfolk/"
-
-# script specific files
-## c
-dir_mrms_hrly = dir_sst_nrflk + "sst_mrms_hourly/"
-f_sst_mrms_hrly = dir_mrms_hrly + "mrms_hourly_template.sst"
-dir_for_sst_files = dir_mrms_hrly + "_inputs/"
-
-## d
-dir_mrms = dir_sst_nrflk + "sst_mrms/"
-f_sst_mrms = dir_mrms + "mrms_template.sst"
-dir_for_sst_files = dir_mrms + "_inputs/"
-
 #%% hard coding
 
 #%% functions
 def c_rainyday_in():
+    dir_mrms_hrly = dir_sst_nrflk + "sst_mrms_hourly/"
+    f_sst_mrms_hrly = dir_mrms_hrly + "mrms_hourly_template.sst"
+    dir_for_sst_files = dir_mrms_hrly + "_inputs/"
     return dir_mrms_hrly, f_sst_mrms_hrly, dir_for_sst_files
 
 def d_rainyday_in():
+    dir_mrms = dir_sst_nrflk + "sst_mrms/"
+    f_sst_mrms = dir_mrms + "mrms_template.sst"
+    dir_for_sst_files = dir_mrms + "_inputs/"
+    return dir_mrms, f_sst_mrms, dir_for_sst_files
+
+def da_rainyday_in():
+    dir_mrms = dir_sst_nrflk + "sst_mrms_subdivided/"
+    f_sst_mrms = dir_mrms + "mrms_template_{}.sst"
+    dir_for_sst_files = dir_mrms + "_inputs/"
     return dir_mrms, f_sst_mrms, dir_for_sst_files
 
 
