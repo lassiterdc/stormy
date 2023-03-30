8fe87c354c4007c31ec41cb0d96b5bdf1e850c69
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-03-30 11:55:54 -0400

    troubleshooting

--- stochastic_storm_transposition/hpc/__directories.sh
+++ stochastic_storm_transposition/hpc/__directories.sh
@@ -39,6 +39,7 @@ assar_dirs[hpc_b_SST_in_hrly_subset2003]=${assar_dirs[sst]}"norfolk/norfolk_mrms
 assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV]=${assar_dirs[sst]}"norfolk/norfolk_stageIV_hourly_subset_short_2003_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV_with_nas]=${assar_dirs[sst]}"norfolk/norfolk_stageIV_with_nas_hourly_subset_short_2003_rivanna.sst"
 assar_dirs[hpc_b_SST_in_hrly_2001_to_2011]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_2001-2011_rivanna.sst"
+assar_dirs[hpc_c1_sst_test]=${assar_dirs[hpc_mrms_hourly]}"mrms_hourly_combined_test.sst"
 
 
 
---
+++ stochastic_storm_transposition/hpc/_test_c1_combining_catalogs_sampling_storms.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -o _script_outputs/%x/%A_%a_%N.out
+#SBATCH -e _script_errors/%x/%A_%a_%N.out
+#SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
+#SBATCH -p standard				# Queue name "standard" (serial)
+#SBATCH -A quinnlab_paid				# allocation name
+#SBATCH -t 6:00:00				# Run time per serial job (hh:mm:ss)
+#SBATCH --array=1       # Array of jobs to loop through 22 years (2001-2011, 15-22)
+#SBATCH --mem-per-cpu=36000
+#SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
+#SBATCH --mail-type=ALL   
+# SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
+
+module purge
+module load anaconda
+source activate rainyday
+
+source __directories.sh
+
+cat ${assar_dirs[hpc_mrms_hourly]}*_20*.nc > ${assar_dirs[hpc_c1_cmbnd_cat]}
+
+echo "Resampling from hourly mrms storm catalog......."
+
+# running RainyDay
+python ${assar_dirs[hpc_rainyday_py]} ${assar_dirs[hpc_c1_sst_test]}
