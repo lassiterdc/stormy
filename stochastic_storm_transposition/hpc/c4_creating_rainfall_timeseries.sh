7058e735fbc252988b5ec644a341b513b8ed4199
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-11 08:36:02 -0400

    tb

--- stochastic_storm_transposition/hpc/c4_creating_rainfall_timeseries.sh
+++ stochastic_storm_transposition/hpc/c4_creating_rainfall_timeseries.sh
@@ -2,9 +2,9 @@
 #SBATCH -o _script_outputs/%x/%A_%a_%N.out
 #SBATCH -e _script_errors/%x/%A_%a_%N.out
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
-#SBATCH -p dev				# Queue name "standard" (serial) [standard]
+#SBATCH -p standard				# Queue name "standard" (serial) [standard]
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 01:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
+#SBATCH -t 00:30:00				# Run time per serial job (hh:mm:ss) [24:00:00]
 #SBATCH --array=1-1000	        # Array of jobs, 1 for each of 1000 years    
 # SBATCH --mem-per-cpu=200000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
