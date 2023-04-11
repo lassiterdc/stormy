acf1217341515e5127a480cd3a27df3659c54acd
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-11 08:44:39 -0400

    initial commit for troubleshooting

--- stochastic_storm_transposition/hpc/__directories.sh
+++ stochastic_storm_transposition/hpc/__directories.sh
@@ -48,5 +48,7 @@ assar_dirs[hpc_b_SST_in_hrly_subset2003_stageIV_with_nas]=${assar_dirs[sst]}"nor
 assar_dirs[hpc_b_SST_in_hrly_2001_to_2011]=${assar_dirs[sst]}"norfolk/norfolk_mrms_hourly_2001-2011_rivanna.sst"
 assar_dirs[hpc_c1_sst_test]=${assar_dirs[hpc_mrms_hourly]}"mrms_hourly_combined_test.sst"
 
+# reprex for Rivanna Support
+assar_dirs[hpc_sup_c4_py]=${assar_dirs[hpc_scripts]}"_support_c4_creating_rainfall_timeseries.py"
 
 
