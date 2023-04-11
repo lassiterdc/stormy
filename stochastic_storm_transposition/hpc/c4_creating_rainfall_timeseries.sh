22f8d7dd363bb1c07fc858ee98424cdc2fe225aa
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-11 11:01:17 -0400

    increasing time limit
    soime tasks actually did complete within 30 minutes

--- stochastic_storm_transposition/hpc/c4_creating_rainfall_timeseries.sh
+++ stochastic_storm_transposition/hpc/c4_creating_rainfall_timeseries.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial) [standard]
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 00:30:00				# Run time per serial job (hh:mm:ss) [24:00:00]
+#SBATCH -t 03:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
 #SBATCH --array=1-1000	        # Array of jobs, 1 for each of 1000 years    
 # SBATCH --mem-per-cpu=200000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
