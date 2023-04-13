198922ac282819ab69924afcde7236f8c8072506
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-13 16:55:26 -0400

    updating for script B

--- stochastic_storm_rescaling/_inputs.py
+++ stochastic_storm_rescaling/_inputs.py
@@ -11,21 +11,28 @@ in_a_begin_year = 2001
 
 # minimum record length to use an NCEI dataset
 min_record_length = 30
+
+# SST stuff
+nrealizations = 1
+
+start_date = "2020-09-01" # start date for each of the time series
 #%% Directories
 fldr_nrflk = str(Path(os.getcwd()).parents[1]) + "/"
-fldr_ssr = fldr_nrflk + "stormy/stochastic_storm_rescaling/"
+fldr_stormy = fldr_nrflk + "stormy/"
+dir_sst = fldr_stormy + "stochastic_storm_transposition/"
+fldr_ssr = fldr_stormy + "stochastic_storm_rescaling/"
 fldr_mrms_processing = fldr_nrflk + "/highres-radar-rainfall-processing/data/"
 dir_ssr_outputs = fldr_ssr + "outputs/"
 
 # SWMM stuff
-fldr_swmm = fldr_nrflk + "stormy/swmm/"
+fldr_swmm = fldr_stormy + "swmm/"
 fldr_swmm_features = fldr_swmm + "hague/swmm_model/exported_layers/"
-fldr_swmm_tseries = fldr_nrflk + "stormy/swmm/hague/swmm_timeseries/"
+fldr_swmm_tseries = fldr_stormy + "swmm/hague/swmm_timeseries/"
 f_swmm_model = fldr_swmm + "hague_V1_using_norfolk_data.inp"
 f_shp_swmm_subs = fldr_swmm_features + "subcatchments.shp"
 
 # NCEI Data
-fldr_NCEI = fldr_nrflk + "stormy/data/climate/NCEI/"
+fldr_NCEI = fldr_stormy + "data/climate/NCEI/"
 f_daily_summaries = fldr_NCEI + "2023-1-4_NCEI_daily summaries_download.csv"
 f_hourlyglobal_precip_all_stations = fldr_NCEI + "2023-1-4_NCEI_Hourly Global_precip.csv"
 f_hourlyglobal_precip_subset_stations = fldr_NCEI + "2023-1-4_NCEI_Hourly Global_station_subset_precip.csv"
@@ -52,6 +59,16 @@ f_mrms_rainfall = fld_out_b + "mrms_rainfall.csv"
 f_out_b_csv_subs_w_mrms_grid = fld_out_b + "b_sub_ids_and_mrms_rain_col.csv"
 f_out_swmm_rainfall = fldr_swmm_tseries + "b_mrms_rainfall_in_per_hr_{}.dat"
 
+f_realizations_hrly = fldr_swmm + "hague/swmm_scenarios_sst_hourly/_combined_realizations.nc"
+
+
+dir_swmm_sst_scenarios_hrly = fldr_swmm + "hague/swmm_scenarios_sst_hourly/"
+dir_time_series_hrly = dir_swmm_sst_scenarios_hrly + "time_series/"
+f_key_subnames_gridind = dir_swmm_sst_scenarios_hrly + "_key_subnames_and_grid-indices.csv"
+
+sst_hrly_tstep_min = 60 # number of minutes per tstep
+
+time_buffer = 6 # hours; this is the amount that must be included before and after peak storm surge while generating the water level time series
 # unique to script c
 fld_out_c_plts = fldr_NCEI + "qaqc_plots/"
 fld_out_c_processed_data = fldr_NCEI + "processed_data/"
@@ -78,4 +95,4 @@ def def_inputs_for_c():
     return f_daily_summaries, f_hourlyprecip, fld_out_c_plts, fld_out_c_processed_data, min_record_length
 
 def def_inputs_for_d():
-    return f_mrms_event_summaries, f_mrms_event_timeseries, f_water_level_storm_surge
+    return f_mrms_event_summaries, f_mrms_event_timeseries, f_water_level_storm_surge, f_realizations_hrly, f_key_subnames_gridind, nrealizations, sst_hrly_tstep_min, start_date, time_buffer, dir_time_series_hrly
