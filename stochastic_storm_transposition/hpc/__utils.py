b4607ba2cb5f4841b4493843ac79b25c2f4a11fb
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-16 21:15:54 -0400

    re-running years with failed model runs due to time limit; increased time limit to 1 hour

--- stochastic_storm_transposition/hpc/__utils.py
+++ stochastic_storm_transposition/hpc/__utils.py
@@ -59,7 +59,7 @@ f_inp_base_hrly = dir_swmm_sst_scenarios_hrly_proj + "hague_sst_model_template.i
 f_swmm_scenarios_catalog = dir_swmm_sst_scenarios_hrly_proj + "swmm_scenario_catalogs/" + "_swmm_scenarios_catalog_yr{}.csv"
 
 # c6
-max_runtime_min_hrly = 25 # maximum minutes of runtime allowable for each SWMM simulation
+max_runtime_min_hrly = 60 # maximum minutes of runtime allowable for each SWMM simulation
 
 # c6b
 f_model_perf_summary_hrly = dir_swmm_sst_scenarios_hrly_proj + "model_performance_summary.csv"
