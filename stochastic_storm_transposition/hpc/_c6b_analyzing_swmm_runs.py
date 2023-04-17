43258aa95d237a57c5047a159e8967f6c3d2c804
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-17 11:30:47 -0400

    moved a function to the __utils.py file

--- stochastic_storm_transposition/hpc/_c6b_analyzing_swmm_runs.py
+++ stochastic_storm_transposition/hpc/_c6b_analyzing_swmm_runs.py
@@ -1,8 +1,9 @@
 #%% import libraries
 import pandas as pd
-from __utils import c6b_analyzing_swmm_runs
+from __utils import c6b_analyzing_swmm_runs, parse_inp
 from glob import glob
 from pathlib import Path
+
 
 dir_swmm_sst_models, f_model_perf_summary = c6b_analyzing_swmm_runs()
 f_performance = dir_swmm_sst_models + "_model_performance_year{}.csv".format("*")
@@ -18,10 +19,15 @@ for f in lst_f_perf:
 
 df_perf = pd.concat(lst_dfs_perf)
 
-# export
+# export performance dataframe
 df_perf.to_csv(f_model_perf_summary)
-#%% analyze
-# df_perf_failed = df_perf[df_perf.run_completed==False]
-# df_perf_succeeded = df_perf[df_perf.run_completed==True]
 
-#%% export consolidated summary
+
+
+
+
+
+
+
+
+
