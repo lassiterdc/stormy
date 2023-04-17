418ea70363d34b1a166b174f5edbf4deb9fbb096
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-04-17 12:43:55 -0400

    remove whitespace

--- stochastic_storm_transposition/hpc/_c7_consolidating_swmm_outputs.py
+++ stochastic_storm_transposition/hpc/_c7_consolidating_swmm_outputs.py
@@ -48,12 +48,5 @@ for f_inp in df_perf_success.swmm_inp:
         lst_ds_node_fld.append(ds)
 
 #%% concatenate the dataset
-
-
 ds_all_node_fld = xr.combine_by_coords(lst_ds_node_fld)
 ds_all_node_fld.to_netcdf(f_out_modelresults, encoding= {"node_flooding":{"zlib":True}})
-
-
-
-
-
