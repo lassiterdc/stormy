6205694f795ce176cff1a20a8c2000f0512b232a
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-06-21 15:48:26 -0400

    modified to create a second SWMM model with a free outfall boundary condition

--- stochastic_storm_transposition/hpc/_c5_creating_inps.py
+++ stochastic_storm_transposition/hpc/_c5_creating_inps.py
@@ -59,77 +59,86 @@ df_strms = pd.DataFrame(dict(storm_id = df_event_summaries.storm_id.values))
 df_key = pd.read_csv(f_key_subnames_gridind)
 
 # print("begin writing {} .inp files...".format(num_files))
+lst_outfall_types = ["TIMESERIES", "FREE"]
 
-with open(f_inp_base, 'r') as T:
-    # loading template
-    template = Template(T.read())
-    count = -1
-    for rz in realization_ids:
-        dir_r = dir_swmm_sst_models_hrly + "weather_realization{}/".format(rz)
-        dir_yr = dir_r + "year{}/".format(yr)
-        # clear folder of any other swmm files from previous runs in case I update the naming convention
-        # try:
-        #     shutil.rmtree(dir_yr)
-        # except:
-        #     pass
-        Path(dir_yr).mkdir(parents=True, exist_ok=True)
-        for storm_id in df_event_summaries.storm_id.values: # this eliminates SST time series with 0 rainfall
-            count += 1
-            df_strms.loc[count, "realization"] = rz
-            df_strms.loc[count, "year"] = yr
-            # df_strms.loc[count, "storm_num"] = storm_id
-            # append new row to pandas dataframe
-            # dic_scen = dict(realization = rz, year = yr, storm = storm_id)
-            # create copy of input file
-            # dir_strm = dir_yr + "rz{}_yr{}_strm{}/".format(rz, yr, storm_id)
-            f_inp_scen = dir_yr + "rz{}_yr{}_strm{}.inp".format(rz, yr, storm_id)
-            df_strms.loc[count, "swmm_inp"] = f_inp_scen
-            ## shutil.copy(f_inp_base, f_inp_scen)
-            df_rain_paths = get_rainfiles(rz, yr, storm_id, df_key)
-            # fill in template stuff
-            df_single_event = df_event_summaries[df_event_summaries.realization_id==rz][df_event_summaries.storm_id==storm_id]
-            df_single_event.reset_index(drop=True, inplace = True)
-            d_fields = {}
-            for key in lst_template_keys:
-                # check if the key is for one of the rainfall time series
-                key_grid = key.split("_")[-1]
-                fpath = None
-                for grid_ind in df_rain_paths.grid_ind:
-                    if grid_ind == key_grid:
-                        fpath = df_rain_paths.rain_dats_fullpath[df_rain_paths.grid_ind == grid_ind].values[0]
-                        # format the filepath
-                        # fpath = fpath.replace("/", "\\")
+for outfall_type in lst_outfall_types:
+    with open(f_inp_base, 'r') as T:
+        # loading template
+        template = Template(T.read())
+        count = -1
+        for rz in realization_ids:
+            dir_r = dir_swmm_sst_models_hrly + "weather_realization{}/".format(rz)
+            dir_yr = dir_r + "year{}/".format(yr)
+            # clear folder of any other swmm files from previous runs in case I update the naming convention
+            # try:
+            #     shutil.rmtree(dir_yr)
+            # except:
+            #     pass
+            Path(dir_yr).mkdir(parents=True, exist_ok=True)
+            for storm_id in df_event_summaries.storm_id.values: # this eliminates SST time series with 0 rainfall
+                count += 1
+                df_strms.loc[count, "realization"] = rz
+                df_strms.loc[count, "year"] = yr
+                # df_strms.loc[count, "storm_num"] = storm_id
+                # append new row to pandas dataframe
+                # dic_scen = dict(realization = rz, year = yr, storm = storm_id)
+                # create copy of input file
+                # dir_strm = dir_yr + "rz{}_yr{}_strm{}/".format(rz, yr, storm_id)
+                # define suffix based on downstream boundary condition
+                if outfall_type == "TIMESERIES":
+                    sfx = ""
+                elif outfall_type == "FREE":
+                    sfx = "_freebndry"
+                f_inp_scen = dir_yr + "rz{}_yr{}_strm{}{}.inp".format(rz, yr, storm_id, sfx)
+                df_strms.loc[count, "swmm_inp"] = f_inp_scen
+                ## shutil.copy(f_inp_base, f_inp_scen)
+                df_rain_paths = get_rainfiles(rz, yr, storm_id, df_key)
+                # fill in template stuff
+                df_single_event = df_event_summaries[df_event_summaries.realization_id==rz][df_event_summaries.storm_id==storm_id]
+                df_single_event.reset_index(drop=True, inplace = True)
+                d_fields = {}
+                for key in lst_template_keys:
+                    # check if the key is for one of the rainfall time series
+                    key_grid = key.split("_")[-1]
+                    fpath = None
+                    for grid_ind in df_rain_paths.grid_ind:
+                        if grid_ind == key_grid:
+                            fpath = df_rain_paths.rain_dats_fullpath[df_rain_paths.grid_ind == grid_ind].values[0]
+                            # format the filepath
+                            # fpath = fpath.replace("/", "\\")
+                            df_strms.loc[count, key] = fpath
+                    if fpath is not None: # meaning, if a filepath associated with the grid index was found
+                        d_fields[key] = fpath
+                    elif key == "water_level":
+                        fpath = get_water_level_series(rz, yr, storm_id)
                         df_strms.loc[count, key] = fpath
-                if fpath is not None: # meaning, if a filepath associated with the grid index was found
-                    d_fields[key] = fpath
-                elif key == "water_level":
-                    fpath = get_water_level_series(rz, yr, storm_id)
-                    df_strms.loc[count, key] = fpath
-                    # fpath = fpath.replace("/", "\\")
-                    d_fields[key] = fpath
-                elif key == "START_DATE":
-                    d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
-                elif key == "START_TIME":
-                    d_fields[key] = df_single_event.event_start.dt.strftime("%H:%M:%S")[0]
-                elif key == "REPORT_START_DATE":
-                    d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
-                elif key == "REPORT_START_TIME":
-                    d_fields[key] = df_single_event.event_start.dt.strftime("%H:%M:%S")[0]
-                elif key == "END_DATE":
-                    d_fields[key] = df_single_event.event_end.dt.strftime('%m/%d/%Y')[0]
-                elif key == "END_TIME":
-                    d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
-                else:
-                    sys.exit("SCRIPT FAILED: {} was not found for filling out the SWMM template.".format(key))
-            new_in = template.safe_substitute(d_fields)
-            new_in = template.substitute(d_fields)
-            # new_file = f_inp_scen
-            new_file_path = Path(f_inp_scen)
-            # create _inputs folder if it doesn't already exist
-            new_file_path.parent.mkdir(parents=True, exist_ok=True)
-            # new_file_path.touch()
-            with open (f_inp_scen, "w+") as f1:
-                f1.write(new_in)
+                        # fpath = fpath.replace("/", "\\")
+                        d_fields[key] = fpath
+                    elif key == "OUTFALL_TYPE":
+                        d_fields[key] = outfall_type
+                    elif key == "START_DATE":
+                        d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
+                    elif key == "START_TIME":
+                        d_fields[key] = df_single_event.event_start.dt.strftime("%H:%M:%S")[0]
+                    elif key == "REPORT_START_DATE":
+                        d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
+                    elif key == "REPORT_START_TIME":
+                        d_fields[key] = df_single_event.event_start.dt.strftime("%H:%M:%S")[0]
+                    elif key == "END_DATE":
+                        d_fields[key] = df_single_event.event_end.dt.strftime('%m/%d/%Y')[0]
+                    elif key == "END_TIME":
+                        d_fields[key] = df_single_event.event_start.dt.strftime('%m/%d/%Y')[0]
+                    else:
+                        sys.exit("SCRIPT FAILED: {} was not found for filling out the SWMM template.".format(key))
+                new_in = template.safe_substitute(d_fields)
+                new_in = template.substitute(d_fields)
+                # new_file = f_inp_scen
+                new_file_path = Path(f_inp_scen)
+                # create _inputs folder if it doesn't already exist
+                new_file_path.parent.mkdir(parents=True, exist_ok=True)
+                # new_file_path.touch()
+                with open (f_inp_scen, "w+") as f1:
+                    f1.write(new_in)
 #%% export swmm catalog to csv file
 # create directory if it doesn't exist
 # pth = Path(f_swmm_scenarios_catalog.format(yr)).mkdir(parents=True, exist_ok=True)
