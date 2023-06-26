4db3af12492718d51f25a0642e1fdeb18ca5c936
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-06-26 10:10:39 -0400

    updating to add dimension of whether or not model had free boundary condition

--- stochastic_storm_transposition/hpc/_c7_consolidating_swmm_outputs.py
+++ stochastic_storm_transposition/hpc/_c7_consolidating_swmm_outputs.py
@@ -28,9 +28,10 @@ df_perf_success = df_perf_success[df_perf_success.year==sim_year]
 
 storm_number = 0
 lst_ds_node_fld = []
+lst_free_bndry = []
 for f_inp in df_perf_success.swmm_inp:
     storm_number += 1
-    rz, yr, storm_id = parse_inp(f_inp)
+    rz, yr, storm_id, freebndry = parse_inp(f_inp)
     f_swmm_out = f_inp.split('.inp')[0] + '.out'
     with Output(f_swmm_out) as out:
         lst_tot_node_flding = []
@@ -44,28 +45,31 @@ for f_inp in df_perf_success.swmm_inp:
             lst_tot_node_flding.append(d_t_series.sum())
             lst_keys.append(key)
         # create array of flooded values with the correct shape for placing in xarray dataset
-        a_fld_reshaped = np.reshape(np.array(lst_tot_node_flding), (1,1,1,len(lst_tot_node_flding))) # rz, yr, storm, node_id
+        a_fld_reshaped = np.reshape(np.array(lst_tot_node_flding), (1,1,1,1,len(lst_tot_node_flding))) # rz, yr, storm, node_id, freeboundary
         # if there is a gap in the models that were run, fill with NA's to make concatenation easier in script c7b
         # (it is essential that coordinates are monotonically increasing).
         while storm_id > storm_number:
             # create dataset with na values with same shape as the flood data
             a_zeros = np.empty(a_fld_reshaped.shape)
             # create dataset with those na values
-            ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'node_id'], a_zeros)),
+            ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'freeboundary', 'node_id'], a_zeros)),
                             coords = dict(realization = np.atleast_1d(rz),
                                             year = np.atleast_1d(yr),
                                             storm_id = np.atleast_1d(storm_number),
+                                            freeboundary = np.atleast_1d(freebndry),
                                             node_id = lst_keys
                                             ))
             # append the dataset to the list of datasets
             lst_ds_node_fld.append(ds)
+            # lst_free_bndry.append(freebndry)
             # increment storm number by 1
             storm_number += 1
         # create dataset with the flood values
-        ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'node_id'], a_fld_reshaped)),
+        ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'freeboundary', 'node_id'], a_fld_reshaped)),
                         coords = dict(realization = np.atleast_1d(rz),
                                         year = np.atleast_1d(yr),
                                         storm_id = np.atleast_1d(storm_id),
+                                        freeboundary = np.atleast_1d(freebndry),
                                         node_id = lst_keys
                                         ))
         # append the dataset to the list of datasets
