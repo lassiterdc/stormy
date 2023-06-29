6a9df0da1ffcd057cab15fda342aa5a4ad1e896e
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-06-29 11:02:13 -0400

    TBS

--- stochastic_storm_transposition/hpc/c4_through_c8b_batch_script.sh
+++ stochastic_storm_transposition/hpc/c4_through_c8b_batch_script.sh
@@ -16,7 +16,8 @@
 
 # ID1=$(sbatch --parsable c5_creating_inps.sh)
 # ID2=$(sbatch --parsable --dependency=afterok:$ID1 c6_running_swmm.sh)
-ID3=$(sbatch --parsable --dependency=afterok:$ID2 c6b_analyzing_swmm_runs.sh)
+# ID3=$(sbatch --parsable --dependency=afterok:$ID2 c6b_analyzing_swmm_runs.sh)
+ID3=$(sbatch --parsable c6b_analyzing_swmm_runs.sh) # DELETE AFTER TROUBLESHOOTING
 ID4=$(sbatch --parsable --dependency=afterok:$ID3 c7_consolidating_swmm_outputs.sh)
 # ID5=$(sbatch --parsable --dependency=afterok:$ID4 c7b_consolidating_swmm_outputs.sh)
 # ID6=$(sbatch --parsable --dependency=afterok:$ID5 c8_bootstrapping_results.sh)
