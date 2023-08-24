38231df2c68982bbb15f246113462c00e4191c00
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-08-24 15:40:40 -0400

    reducing memory allocations

--- stochastic_storm_transposition/hpc/d_running_rainyday_mrms.sh
+++ stochastic_storm_transposition/hpc/d_running_rainyday_mrms.sh
@@ -6,15 +6,14 @@
 #SBATCH -A quinnlab_paid				# allocation name
 #SBATCH -t 168:00:00				# Run time per serial job (hh:mm:ss)
 #SBATCH --array=1-11,15-22	        # Array of jobs to loop through 22 years (2001-2011, 15-22)
-#SBATCH --mem-per-cpu=200000
+#SBATCH --mem-per-cpu=80000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
-#SBATCH --mail-type=ALL   
+#SBATCH --mail-type=ALL
 # SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
 
 module purge
 module load anaconda
 source activate rainyday
-
 source __directories.sh
 
 # assign year variable
