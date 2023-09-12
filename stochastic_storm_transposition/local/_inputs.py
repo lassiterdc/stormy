c85ae1f4074d56bd486550dc3f5c0fe761facd0b
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-09-12 14:44:46 -0400

    updating filepaths

--- stochastic_storm_transposition/local/_inputs.py
+++ stochastic_storm_transposition/local/_inputs.py
@@ -11,10 +11,11 @@ f_shp_wshed = fldr_rainyday_working_dir + "watershed/norfolk_wshed_4326.shp"
 f_shp_trans_dom = fldr_rainyday_working_dir + "transposition_domain/norfolk_trans_dom_4326.shp"
 
 # work_a
-scen_name = "sst_mrms_hourly"
-fldr_rainyday_outputs = fldr_rainyday_working_dir + "{}/".format(scen_name)
-# f_csv_freq_analysis = fldr_rainyday_outputs + "{}_FreqAnalysis.csv".format(scen_name)
-# fldr_realizations = fldr_rainyday_outputs + "Realizations/"
+
+scen_name = "mrms_combined"
+fldr_rainyday_outputs = fldr_rainyday_working_dir + "sst_mrms/{}/".format(scen_name)
+f_csv_freq_analysis = fldr_rainyday_outputs + "{}_FreqAnalysis.csv".format(scen_name)
+fldr_realizations = fldr_rainyday_outputs + "Realizations/"
 # f_nc_storm_cat = fldr_rainyday_working_dir + "norfolk_mrms_sst_subset_rivanna.nc"
 
 
@@ -23,6 +24,3 @@ fldr_rainyday_outputs = fldr_rainyday_working_dir + "{}/".format(scen_name)
 # def work_a_inspctng_strm_cat():
 #     return f_nc_storm_cat, f_csv_freq_analysis, fldr_realizations, f_shp_wshed, f_shp_trans_dom
 
-
-def a_stormat_processing():
-    return fldr_rainyday_outputs
