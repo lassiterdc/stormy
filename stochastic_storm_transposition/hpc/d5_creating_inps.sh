d1602d07c00d5e8ae8c74ca14c398fdaac968eba
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-09-12 15:44:38 -0400

    adding stuff to ensure no issues with GIS tools

--- stochastic_storm_transposition/hpc/d5_creating_inps.sh
+++ stochastic_storm_transposition/hpc/d5_creating_inps.sh
@@ -11,11 +11,16 @@
 #SBATCH --mail-type=ALL   
 # SBATCH --exclude=udc-ba26-18,udc-ba27-14,udc-ba26-16,udc-ba26-17
 
-module purge
-module load gcc openmpi eccodes anaconda
-source activate rainyday
+# ijob -c 1 -A quinnlab_paid -p standard --time=0-06:00:00 --mem-per-cpu=64000
 
 source __directories.sh
+module purge
+module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
+DIR=~/.conda/envs/rainyday
+source activate rainyday
+export PATH=$DIR/bin:$PATH
+export LD_LIBRARY_PATH=$DIR/lib:$PATH
+export PYTHONPATH=$DIR/lib/python3.11/site-packages:$PATH
 
 # echo "Creating SWMM models for year ${SLURM_ARRAY_TASK_ID}..."
 
