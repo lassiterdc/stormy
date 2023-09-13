83f78b1430f1a3c52827c7e6f1270b60736006ea
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-09-13 14:41:47 -0400

    tbs

--- stochastic_storm_transposition/hpc/d4c1_analyzing_weather_consolidation.sh
+++ stochastic_storm_transposition/hpc/d4c1_analyzing_weather_consolidation.sh
@@ -5,23 +5,24 @@
 #SBATCH -p standard				# Queue name "standard" (serial) [standard]
 #SBATCH -A quinnlab_paid				# allocation name
 #SBATCH -t 12:00:00
-# SBATCH --mem-per-cpu=200000				# Run time per serial job (hh:mm:ss) [24:00:00]
 #SBATCH --array=1  # these are the jobs with errors; normally run 1-1000 unless re-running failed scripts        # Array of jobs, 1 for each of 1000 years  
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
+# SBATCH --mem-per-cpu=200000				# Run time per serial job (hh:mm:ss) [24:00:00]
 
 # ijob -c 1 -A quinnlab_paid -p standard --time=0-06:00:00 --mem-per-cpu=64000
 
 source __directories.sh
 module purge
-module load gcc openmpi eccodes anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
-DIR=~/.conda/envs/rainyday
+module load anaconda # the stuff other than anaconda was to ensure eccodes loaded correctly
+# DIR=~/.conda/envs/rainyday
 source activate rainyday
-export PATH=$DIR/bin:$PATH
-export LD_LIBRARY_PATH=$DIR/lib:$PATH
-export PYTHONPATH=$DIR/lib/python3.11/site-packages:$PATH
+# export PATH=$DIR/bin:$PATH
+# export LD_LIBRARY_PATH=$DIR/lib:$PATH
+# export PYTHONPATH=$DIR/lib/python3.11/site-packages:$PATH
 
 # echo "Creating rainfall time series for year ${SLURM_ARRAY_TASK_ID}..."
 
 # running script
+echo "running script ${assar_dirs[hpc_d4c1_py]}"
 python ${assar_dirs[hpc_d4c1_py]}
