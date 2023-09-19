592595742e976a7291e3100f8bb1bd9e4efffed3
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-09-19 15:40:56 -0400

    dividing consolidation of rain and water level to 2 scripts

--- stochastic_storm_transposition/hpc/_d4c1b_analyzing_weather_consolidation.py
+++ stochastic_storm_transposition/hpc/_d4c1b_analyzing_weather_consolidation.py
@@ -1,3 +1,6 @@
+"""
+This script consolidates all of the water level time series
+"""
 #%% import libraries
 from pathlib import Path
 import xarray as xr
@@ -8,37 +11,7 @@ import shutil
 from __utils import *
 
 start_time = time.time()
-#%%
-lst_f_ncs = glob(fldr_realizations+"*.nc")
-bm_time = time.time()
-ds_rlztns = xr.open_mfdataset(lst_f_ncs, preprocess = define_dims, engine='h5netcdf')
-ds_rlztns.attrs["rain_units"] = "mm_per_hour"
-print("Total time elapsed: {}; time to run open_mfdataset on realizations: {}".format(time.time() - start_time, time.time() - bm_time))
 
-#%% writing to zarr
-bm_time = time.time()
-fl_out_zar = dir_zarr_weather_scratch+"weather_combined.zarr"
-ds_rlztns = ds_rlztns.chunk(chunks={"realization": 1, "year": 1, "storm_id": 1, "time": 864, "latitude": 2, "longitude": 3})
-ds_rlztns.to_zarr(fl_out_zar, mode="w")
-print("Total time elapsed: {}; time to export combined rainfall realizations to zarr: {}".format(time.time() - start_time, time.time() - bm_time))
-
-# Load zarr and export to netcdf file
-bm_time = time.time()
-ds_from_zarr = xr.open_zarr(store=fl_out_zar, chunks={'year':"5000MB"})
-ds_from_zarr.to_netcdf(f_rain_realizations, encoding= {"rain":{"zlib":True}})
-# delete zarr file
-shutil.rmtree(fl_out_zar)
-print("Total time elapsed: {}; time to export combined rainfall realizations to netcdf and delete Zarr: {}".format(time.time() - start_time, time.time() - bm_time))
-
-#%% write all rainfall realizations directly to a new netcdf file
-# bm_time = time.time()
-# ds_rlztns_loaded = ds_rlztns.load()
-# print("Total time elapsed: {}; time to load dataset into memory: {}".format(time.time() - start_time, time.time() - bm_time))
-# # create path if non-existant
-# Path(f_rain_realizations).parent.mkdir(parents=True, exist_ok=True)
-# bm_time = time.time()
-# ds_rlztns_loaded.to_netcdf(f_rain_realizations, encoding= {"rain":{"zlib":True}})
-# print("Total time elapsed: {}; time to export combined realizations to netcdf: {}".format(time.time() - start_time, time.time() - bm_time))
 #%% water levels
 f_summary = dir_time_series + "_event_summary_year{}.csv".format('*') # must match format in script d4b
 lst_f_summaries = glob(f_summary)
