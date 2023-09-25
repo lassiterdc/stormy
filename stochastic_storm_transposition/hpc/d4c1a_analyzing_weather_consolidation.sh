84090608da49f5e03d97aa56afe0bcb5a2137f2a
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-09-25 11:07:59 -0400

    increasing time decreasing memory

--- stochastic_storm_transposition/hpc/d4c1a_analyzing_weather_consolidation.sh
+++ stochastic_storm_transposition/hpc/d4c1a_analyzing_weather_consolidation.sh
@@ -4,10 +4,10 @@
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial) [standard]
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 24:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
+#SBATCH -t 48:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
-#SBATCH --mem-per-cpu=32000
+#SBATCH --mem-per-cpu=16000
 
 # ijob -c 1 -A quinnlab_paid -p standard --time=0-06:00:00 --mem-per-cpu=64000
 
