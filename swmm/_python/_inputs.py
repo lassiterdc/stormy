1f0ba22d9edbcc106b268b9ad5cf4cfeb25fbf15
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-10-30 18:28:22 -0400

    commending out unneeded lines

--- swmm/_python/_inputs.py
+++ swmm/_python/_inputs.py
@@ -61,17 +61,17 @@ f_bootstrapping_analysis = fldr_swmm_analysis + "df_comparison_bootstrapped.csv"
 ## plots 
 dir_plots = dir_outputprocessing + "_plots/"
 # define functions for scripts
-def a_analyzing_sst_results():
-    return f_sst_results_hrly, f_model_perf_summary, f_sst_event_summaries, f_sst_annual_max_volumes, volume_units
+# def a_analyzing_sst_results():
+#     return f_sst_results_hrly, f_model_perf_summary, f_sst_event_summaries, f_sst_annual_max_volumes, volume_units
 
-def b_processing_design_strms():
-    return dir_swmm_design_storms, f_design_strms, cubic_feet_per_cubic_meter, volume_units, mm_per_inch, lst_outfall_ids, sub_id_for_rain
+# def b_processing_design_strms():
+#     return dir_swmm_design_storms, f_design_strms, cubic_feet_per_cubic_meter, volume_units, mm_per_inch, lst_outfall_ids, sub_id_for_rain
 
-def c_ams_with_sst():
-    return f_sst_annual_max_volumes, ks_alpha, bootstrap_iterations, volume_units, f_sst_recurrence_intervals, sst_conf_interval, sst_recurrence_intervals
+# def c_ams_with_sst():
+#     return f_sst_annual_max_volumes, ks_alpha, bootstrap_iterations, volume_units, f_sst_recurrence_intervals, sst_conf_interval, sst_recurrence_intervals
 
-def d_comparing_design_strms_and_sst():
-    return f_sst_annual_max_volumes, f_design_strms, f_sst_recurrence_intervals, dir_plots, m_per_feet, sst_recurrence_intervals, size_scaling_factor, target_text_size_in, pts_per_inch
+# def d_comparing_design_strms_and_sst():
+#     return f_sst_annual_max_volumes, f_design_strms, f_sst_recurrence_intervals, dir_plots, m_per_feet, sst_recurrence_intervals, size_scaling_factor, target_text_size_in, pts_per_inch
 
 # def e_isolating_surge_effects():
 #     return f_sst_results_hrly, sst_conf_interval, sst_recurrence_intervals, f_bootstrap_hrly,f_bootstrap_raw_hrly, f_shp_jxns, f_shp_strg, f_shp_out, f_shp_coast, f_shp_subs
