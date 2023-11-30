ca38376dcb4b5c342c5dba7bcb386152cf8b105d
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-11-30 17:55:50 -0500

    reducing time

--- stochastic_storm_transposition/hpc/d4c_combining_wlevels.sh
+++ stochastic_storm_transposition/hpc/d4c_combining_wlevels.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1				# Number of tasks per serial job (must be 1)
 #SBATCH -p standard				# Queue name "standard" (serial) [standard]
 #SBATCH -A quinnlab_paid				# allocation name
-#SBATCH -t 48:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
+#SBATCH -t 4:00:00				# Run time per serial job (hh:mm:ss) [24:00:00]
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
 #SBATCH --mem-per-cpu=16000
