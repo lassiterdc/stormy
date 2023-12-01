5e2f4fd52fa0a27e826919787358598841a31a70
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-11-30 19:25:40 -0500

    update batch script names

--- stochastic_storm_transposition/hpc/d5_through_d8b_batch_script.sh
+++ stochastic_storm_transposition/hpc/d5_through_d8b_batch_script.sh
@@ -2,10 +2,10 @@
 # chmod +x c5__through_c8b_batch_script.sh
 # ./c5__through_c8b_batch_script.sh
 
-ID1=$(sbatch --parsable c5_creating_inps.sh)
-ID2=$(sbatch --parsable --dependency=afterok:$ID1 c6_running_swmm.sh)
-ID3=$(sbatch --parsable --dependency=afterok:$ID2 c6b_analyzing_swmm_runs.sh)
-ID4=$(sbatch --parsable --dependency=afterok:$ID3 c7_consolidating_swmm_outputs.sh)
-ID5=$(sbatch --parsable --dependency=afterok:$ID4 c7b_consolidating_swmm_outputs.sh)
-ID6=$(sbatch --parsable --dependency=afterok:$ID5 c8_bootstrapping_results.sh)
-ID7=$(sbatch --parsable --dependency=afterok:$ID6 c8b_consolidating_bs_samples.sh)
+ID1=$(sbatch --parsable d5_creating_inps.sh)
+ID2=$(sbatch --parsable --dependency=afterok:$ID1 d6_running_swmm.sh)
+ID3=$(sbatch --parsable --dependency=afterok:$ID2 d6b_analyzing_swmm_runs.sh)
+ID4=$(sbatch --parsable --dependency=afterok:$ID3 d7_consolidating_swmm_outputs.sh)
+ID5=$(sbatch --parsable --dependency=afterok:$ID4 d7b_consolidating_swmm_outputs.sh)
+ID6=$(sbatch --parsable --dependency=afterok:$ID5 d8_bootstrapping_results.sh)
+ID7=$(sbatch --parsable --dependency=afterok:$ID6 d8b_consolidating_bs_samples.sh)
