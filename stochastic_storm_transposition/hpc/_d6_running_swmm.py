d473ecfa042bdd98ea1ee995f4ecd03f035ff699
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-12-03 12:35:45 -0500

    adding more benchmarking

--- stochastic_storm_transposition/hpc/_d6_running_swmm.py
+++ stochastic_storm_transposition/hpc/_d6_running_swmm.py
@@ -64,6 +64,8 @@ lst_outputs_converted_to_netcdf = [] # to track success
 f_out_modelresults = dir_swmm_sst_models + "_model_outputs_year{}.nc".format(sim_year)
 # END DCL WORK
 runtimes = []
+export_dataset_times_min = []
+# export_dataset_times = []
 successes = []
 problems = []
 count = -1
@@ -77,7 +79,7 @@ for idx, row in df_strms.iterrows():
     print("Running simulation for realization {} year {} storm {}. {} out of {} simulations complete.".format(rz, yr, storm_id, count, s_tot_sims))
     success = True
     output_converted_to_netcdf = False
-    sim_time = datetime.now()
+    loop_start_time = sim_time = datetime.now()
     sim_runtime_min = np.nan
     # break
     try:
@@ -98,14 +100,12 @@ for idx, row in df_strms.iterrows():
         success = False
     problems.append(problem)
     successes.append(success)
-    tot_elapsed_time_hr = round((sim_time - script_start_time).seconds / 60 / 60, 1)
-    runtimes.append(sim_runtime_min)
-    mean_sim_time = round(np.nanmean(runtimes), 1)
-    expected_tot_runtime_hr = round(mean_sim_time*s_tot_sims/60, 1)
-    expected_remaining_time_hr = round((expected_tot_runtime_hr - tot_elapsed_time_hr), 1)
+    # benchmarking write netcdf
+    start_create_dataset = datetime.now()
+    create_dataset_time_min = np.nan
     # MONTIROING
     if success == True:
-        print("Simulation runtime (min): {}, Mean simulation runtime (min): {}, Total elapsed time (hr): {}, Expected total time (hr): {}, Estimated time remaining (hr): {}".format(sim_runtime_min, mean_sim_time, tot_elapsed_time_hr, expected_tot_runtime_hr, expected_remaining_time_hr)) 
+        # print("Simulation runtime (min): {}, Mean simulation runtime (min): {}, Total elapsed time (hr): {}, Expected total time (hr): {}, Estimated time remaining (hr): {}".format(sim_runtime_min, mean_sim_time, tot_elapsed_time_hr, expected_tot_runtime_hr, expected_remaining_time_hr)) 
         #%% dcl work  - incorporating processing of outputs into the script
         print("Exporting node flooding as netcdfs....")
         rz, yr, storm_id, freebndry, norain = parse_inp(f_inp)
@@ -121,20 +121,43 @@ for idx, row in df_strms.iterrows():
                 # sum all flooded volumes and append lists
                 lst_tot_node_flding.append(d_t_series.sum())
                 lst_keys.append(key)
-            # create array of flooded values with the correct shape for placing in xarray dataset
-            a_fld_reshaped = np.reshape(np.array(lst_tot_node_flding), (1,1,1,1,1,len(lst_tot_node_flding))) # rz, yr, storm, node_id, freeboundary, norain
-            # create dataset with the flood values 
-            ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'freeboundary', 'norain', 'node_id'], a_fld_reshaped)),
-                            coords = dict(realization = np.atleast_1d(rz),
-                                            year = np.atleast_1d(yr),
-                                            storm_id = np.atleast_1d(storm_id),
-                                            freeboundary = np.atleast_1d(freebndry),
-                                            norain = np.atleast_1d(norain),
-                                            node_id = lst_keys
-                                            ))
-            lst_ds_node_fld.append(ds)
-            lst_f_outputs_converted_to_netcdf.append(f_swmm_out)
-            output_converted_to_netcdf = True
+        # create array of flooded values with the correct shape for placing in xarray dataset
+        a_fld_reshaped = np.reshape(np.array(lst_tot_node_flding), (1,1,1,1,1,len(lst_tot_node_flding))) # rz, yr, storm, node_id, freeboundary, norain
+        # create dataset with the flood values 
+        ds = xr.Dataset(data_vars=dict(node_flooding_cubic_meters = (['realization', 'year', 'storm_id', 'freeboundary', 'norain', 'node_id'], a_fld_reshaped)),
+                        coords = dict(realization = np.atleast_1d(rz),
+                                        year = np.atleast_1d(yr),
+                                        storm_id = np.atleast_1d(storm_id),
+                                        freeboundary = np.atleast_1d(freebndry),
+                                        norain = np.atleast_1d(norain),
+                                        node_id = lst_keys
+                                        ))
+        lst_ds_node_fld.append(ds)
+        lst_f_outputs_converted_to_netcdf.append(f_swmm_out)
+        output_converted_to_netcdf = True
+        print("created xarray dataset with total flooding for each node")
+        end_create_dataset = datetime.now()
+        create_dataset_time_min = round((end_create_dataset - start_create_dataset).seconds / 60, 1)
+    # benchmarking export netcdf
+    export_dataset_times_min.append(create_dataset_time_min)
+    mean_export_ds_time_min = round(np.nanmean(export_dataset_times_min), 1)
+    # benchmarking simulations
+    runtimes.append(sim_runtime_min)
+    mean_sim_time_min = round(np.nanmean(runtimes), 1)
+    # benchmarking entire loop and script
+    ## estimating time remaining assuming successes
+    estimated_loop_time = (mean_sim_time_min+mean_export_ds_time_min)
+    expected_tot_runtime_hr = round(estimated_loop_time*s_tot_sims/60, 1)
+
+    tot_elapsed_time_hr = round((datetime.now() - script_start_time).seconds / 60 / 60, 1)
+    # tot_loop_time_hr = round((datetime.now() - loop_start_time).seconds / 60 / 60, 1)
+    
+    expected_remaining_time_hr = round((expected_tot_runtime_hr - tot_elapsed_time_hr), 1)
+    if success == True:
+        print("Sim runtime (min): {}, Mean sim runtime (min): {}, Time to create dataset (min): {}, Total script time (hr): {}, Expected total time (hr): {}, Estimated time remaining (hr): {}".format(sim_runtime_min, mean_sim_time_min,
+                                                                                                                                                                                                 create_dataset_time_min,
+                                                                                                                                                                                      tot_elapsed_time_hr, expected_tot_runtime_hr,
+                                                                                                                                                                                        expected_remaining_time_hr)) 
         #%% end dcl work
     else: 
         print("Simulation failed after {} minutes.".format(sim_runtime_min))
@@ -144,8 +167,10 @@ for idx, row in df_strms.iterrows():
 df_strms["run_completed"] = successes
 df_strms["problem"] = problems
 df_strms["runtime_min"] = runtimes
+df_strms["export_dataset_min"] = export_dataset_times_min
 df_strms["lst_outputs_converted_to_netcdf"] = lst_outputs_converted_to_netcdf
-
+df_strms.to_csv(f_out_runtimes, index=False)
+print('Exported ' + f_out_runtimes)
 #%% dcl work - incorporating processing of outputs into the script
 ds_all_node_fld = xr.combine_by_coords(lst_ds_node_fld)
 
@@ -154,12 +179,16 @@ ds_all_node_fld_loaded.to_netcdf(f_out_modelresults, encoding= {"node_flooding_c
 
 tot_elapsed_time_min = round((datetime.now() - script_start_time).seconds / 60, 1)
 
+print("exported " + f_out_modelresults)
+
 # remove processed outputs
+print("Removing output files.....")
 for f_processed_output in lst_f_outputs_converted_to_netcdf:
     os.remove(f_processed_output)
-print("Total script runtime (min): {}".format(tot_elapsed_time_min))
-print("Removing output files.....")
+    print("removed file {}".format(f_processed_output))
+
+
 #%% end dcl work
+print("Total script runtime (min): {}".format(tot_elapsed_time_min))
 
 
-df_strms.to_csv(f_out_runtimes, index=False)
