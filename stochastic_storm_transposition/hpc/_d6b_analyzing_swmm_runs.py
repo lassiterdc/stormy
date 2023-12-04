987d106917f02abfa2f7a48b6d548fb1b5422571
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-12-04 14:24:14 -0500

    adding code to  update performance csv with re-run results

--- stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
+++ stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
@@ -8,16 +8,31 @@ from pathlib import Path
 f_performance = dir_swmm_sst_models + "_model_performance_year{}.csv".format("*")
 # f_summaries = dir_time_series + "_event_summary_year{}.csv".format("*")
 f_summaries = f_swmm_scenarios_catalog.format("*")
-
+f_performance_reruns = dir_swmm_sst_models + "_model_performance_year{}_failed_run_id{}.csv".format("*", "*")
 
 #%% load performance dataframe
 lst_f_perf = glob(f_performance)
+lst_f_perf_reruns = glob(f_performance_reruns)
 lst_dfs_perf = []
 for f in lst_f_perf:
+    if "failed_run" in f:
+        continue
     df = pd.read_csv(f)
     lst_dfs_perf.append(df)
-
 df_perf = pd.concat(lst_dfs_perf)
+df_perf = df_perf.set_index(["swmm_inp"], drop=True)
+
+# update the consolidated performance summary with the re-runs
+lst_dfs_perf_reruns = []
+for f in lst_f_perf_reruns:
+    df_reruns = pd.read_csv(f)
+    lst_dfs_perf_reruns.append(df_reruns)
+df_perf_reruns = pd.concat(lst_dfs_perf_reruns)
+df_perf_reruns = df_perf_reruns.set_index(["swmm_inp"], drop=True)
+
+for ind, row in df_perf_reruns.iterrows():
+    df_perf.loc[ind, :] = row
+
 
 # export performance dataframe
 df_perf.to_csv(f_model_perf_summary, index=False)
