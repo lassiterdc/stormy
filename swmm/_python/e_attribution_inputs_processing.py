e5ab74042cfe87adf6637269a153acf6ef88ff4a
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-12-06 09:14:31 -0500

    updating variable names for full resolution results; minor bug fixes

--- swmm/_python/e_attribution_inputs_processing.py
+++ swmm/_python/e_attribution_inputs_processing.py
@@ -1,4 +1,5 @@
 #%% load libraries and directories
+# environment: flood_attribution_py2
 import xarray as xr
 import pandas as pd
 import numpy as np
@@ -24,9 +25,9 @@ scratch_file = "_scratch/{}"
 cmap = "gist_rainbow"
 
 #%%
-ds_sst = xr.open_dataset(f_sst_results_hrly)
-ds_bootstrap_rtrn = xr.open_dataset(f_bootstrap_hrly)
-# ds_bootstrap_raw = xr.open_dataset(f_bootstrap_raw_hrly, chunks=dict(node_id=1))
+ds_sst = xr.open_dataset(f_sst_results)
+ds_bootstrap_rtrn = xr.open_dataset(f_bootstrap)
+# ds_bootstrap_raw = xr.open_dataset(f_bootstrap_raw, chunks=dict(node_id=1))
 # df_model_perf = pd.read_csv(f_model_perf_summary)
 df_sst_events = pd.read_csv(f_sst_event_summaries)
 
@@ -47,8 +48,13 @@ gdf_coast = gdf_coast.to_crs(proj)
 
 
 #%% preprocessing
-ds_sst_compound = ds_sst.sel(freeboundary="False")
-ds_sst_freebndry = ds_sst.sel(freeboundary="True")
+ds_sst_compound = ds_sst.sel(freeboundary="False", norain = "False")
+ds_sst_nosurge = ds_sst.sel(freeboundary="True", norain = "False")
+ds_sst_norain = ds_sst.sel(freeboundary="False", norain = "True")
+
+# for now, using the no surge time series to compute flood attribution
+ds_sst_freebndry = ds_sst_nosurge
+
 ds_fld_dif = ds_sst_compound - ds_sst_freebndry
 
 ds_flood_attribution = 1 - (ds_sst_freebndry + .0000000000001) / (ds_sst_compound + .0000000000001)
@@ -56,7 +62,7 @@ ds_flood_attribution = ds_flood_attribution.rename(dict(node_flooding_cubic_mete
 
 ds_flood_attribution["flood_attribution"]  =xr.where(ds_flood_attribution.flood_attribution < 0, 0, ds_flood_attribution.flood_attribution)
 
-ds_events =df_sst_events.set_index(["realization", "year", "storm_id"]).to_xarray()
+# ds_events =df_sst_events.set_index(["realization", "year", "storm_id"]).to_xarray()
 
 
 
@@ -119,10 +125,10 @@ df_bs_CIs.to_csv(scratch_file.format("df_bootstrapped_CIs.csv"), index=False)
 #%% create dataframe of all events within bootstrap confidence interval
 df_bs_CIs = pd.read_csv(scratch_file.format("df_bootstrapped_CIs.csv"))
 
-df_bs_CIs.groupby("node")
+df_bs_CIs.groupby("node_id")
 
 lst_ds_comp = []
-for node in tqdm(df_bs_CIs.node.unique()):
+for node in tqdm(df_bs_CIs.node_id.unique()):
     count = -1
     for flood_return_yrs in ds_bootstrap_rtrn.flood_return_yrs.values:
         count += 1
@@ -134,13 +140,13 @@ for node in tqdm(df_bs_CIs.node.unique()):
         # df_closest_events = df_sst_compound_subset.iloc[(df_sst_compound_subset['node_flooding_cubic_meters']-node_flooding).abs().argsort()[:min_storms_to_analyze]]
         # filter events
 
-        df_bs_CIs_subset = df_bs_CIs[(df_bs_CIs.node == node) & (df_bs_CIs.flood_return_yrs==flood_return_yrs)]
+        df_bs_CIs_subset = df_bs_CIs[(df_bs_CIs.node_id == node) & (df_bs_CIs.flood_return_yrs==flood_return_yrs)].set_index(["node_id", "flood_return_yrs"])
 
-        upper_bound = float(df_bs_CIs_subset["upper_CI"])
+        upper_bound = float(df_bs_CIs_subset["upper_CI"].values[0])
         if upper_bound <= 0: # if the upper bound is 0, skip the return period 
             continue
         upper_cond = df_sst_compound_subset.node_flooding_cubic_meters.values <= upper_bound
-        lower_bound = float(df_bs_CIs_subset["lower_CI"])
+        lower_bound = float(df_bs_CIs_subset["lower_CI"].values[0])
         lower_cond = ((df_sst_compound_subset.node_flooding_cubic_meters.values >= lower_bound) & (df_sst_compound_subset.node_flooding_cubic_meters.values > 0)) # don't want to include events with no flooding
 
         df_closest_events_filtered = df_sst_compound_subset[(upper_cond) & (lower_cond)]
@@ -153,20 +159,20 @@ for node in tqdm(df_bs_CIs.node.unique()):
 
         df_compare['flood_return_yrs'] = flood_return_yrs
 
-        df_frac_wlevel_var = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.var().reset_index().rename(dict(frac_wlevel = "frac_wlevel_var"), axis = 1)
-        df_frac_wlevel_mean = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.mean().reset_index().rename(dict(frac_wlevel = "frac_wlevel_mean"), axis = 1)
-        df_frac_wlevel_min = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.min().reset_index().rename(dict(frac_wlevel = "frac_wlevel_min"), axis = 1)
-        df_frac_wlevel_max = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.max().reset_index().rename(dict(frac_wlevel = "frac_wlevel_max"), axis = 1)
-        df_frac_wlevel_median = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.median().reset_index().rename(dict(frac_wlevel = "frac_wlevel_median"), axis = 1)
+        df_frac_wlevel_var = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.var().reset_index().rename(dict(frac_wlevel = "frac_wlevel_var"), axis = 1).set_index(["node_id", "flood_return_yrs"])
+        df_frac_wlevel_mean = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.mean().reset_index().rename(dict(frac_wlevel = "frac_wlevel_mean"), axis = 1).set_index(["node_id", "flood_return_yrs"])
+        df_frac_wlevel_min = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.min().reset_index().rename(dict(frac_wlevel = "frac_wlevel_min"), axis = 1).set_index(["node_id", "flood_return_yrs"])
+        df_frac_wlevel_max = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.max().reset_index().rename(dict(frac_wlevel = "frac_wlevel_max"), axis = 1).set_index(["node_id", "flood_return_yrs"])
+        df_frac_wlevel_median = df_compare.groupby(["node_id", "flood_return_yrs"]).frac_wlevel.median().reset_index().rename(dict(frac_wlevel = "frac_wlevel_median"), axis = 1).set_index(["node_id", "flood_return_yrs"])
 
-
-        df_attribution_stats = pd.merge(df_frac_wlevel_var, [df_frac_wlevel_mean, df_frac_wlevel_min,df_frac_wlevel_max, df_frac_wlevel_median], on = ["node_id", "flood_return_yrs"])
+        df_attribution_stats = df_frac_wlevel_var.join([df_frac_wlevel_mean, df_frac_wlevel_min,df_frac_wlevel_max, df_frac_wlevel_median])
         
-        df_out = pd.merge(df_bs_CIs_subset, df_attribution_stats, left_on = ["node", "flood_return_yrs"], right_on = ["node_id", "flood_return_yrs"])
+        df_out = df_attribution_stats.join(df_bs_CIs_subset)
+        # break
         
         df_out["num_strms"] = df_compare.shape[0]
 
         lst_ds_comp.append(df_out)
 
 df_comparison = pd.concat(lst_ds_comp)
-df_comparison.to_csv(f_bootstrapping_analysis, index=False)
+df_comparison.to_csv(f_bootstrapping_analysis, index=True)
