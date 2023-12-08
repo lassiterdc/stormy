8406f1ca539135d645f556c4e420be2cc7a2df9e
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2023-12-08 15:40:38 -0500

    mostly moving code to _inputs file

--- swmm/_python/e_attribution_inputs_processing.py
+++ swmm/_python/e_attribution_inputs_processing.py
@@ -24,76 +24,6 @@ scratch_file = "_scratch/{}"
 
 cmap = "gist_rainbow"
 
-#%%
-ds_sst = xr.open_dataset(f_sst_results)
-ds_bootstrap_rtrn = xr.open_dataset(f_bootstrap)
-# ds_bootstrap_raw = xr.open_dataset(f_bootstrap_raw, chunks=dict(node_id=1))
-# df_model_perf = pd.read_csv(f_model_perf_summary)
-df_sst_events = pd.read_csv(f_sst_event_summaries)
-
-# load and transform shapefile
-gdf_jxns = gpd.read_file(f_shp_jxns)
-gdf_strg = gpd.read_file(f_shp_strg)
-gdf_out = gpd.read_file(f_shp_out)
-gdf_nodes = pd.concat([gdf_jxns, gdf_strg, gdf_out]).loc[:, ["NAME", "geometry"]]
-gdf_subs = gpd.read_file(f_shp_subs)
-gdf_coast = gpd.read_file(f_shp_coast)
-
-proj = ccrs.PlateCarree()
-gdf_nodes = gdf_nodes.to_crs(proj)
-gdf_subs = gdf_subs.to_crs(proj)
-gdf_coast = gdf_coast.to_crs(proj)
- 
-# also load weather statistics
-
-
-#%% preprocessing
-ds_sst_compound = ds_sst.sel(freeboundary="False", norain = "False")
-ds_sst_nosurge = ds_sst.sel(freeboundary="True", norain = "False")
-ds_sst_norain = ds_sst.sel(freeboundary="False", norain = "True")
-
-# for now, using the no surge time series to compute flood attribution
-ds_sst_freebndry = ds_sst_nosurge
-
-ds_fld_dif = ds_sst_compound - ds_sst_freebndry
-
-ds_flood_attribution = 1 - (ds_sst_freebndry + .0000000000001) / (ds_sst_compound + .0000000000001)
-ds_flood_attribution = ds_flood_attribution.rename(dict(node_flooding_cubic_meters = "flood_attribution"))
-
-ds_flood_attribution["flood_attribution"]  =xr.where(ds_flood_attribution.flood_attribution < 0, 0, ds_flood_attribution.flood_attribution)
-
-# ds_events =df_sst_events.set_index(["realization", "year", "storm_id"]).to_xarray()
-
-
-
-#%% exploring questions
-# what is the 1, 2, 5, 10, 25, 50, and 100 year flood at each node?
-def return_period_to_quantile(ds, return_pds):
-    storms_per_year = ds.storm_id.shape[0]
-    total_years = ds.year.shape[0]
-    total_events = storms_per_year * total_years
-    quants = []
-    for return_pd in return_pds:
-        expected_num_of_storms = total_years / return_pd
-        quant = 1 - expected_num_of_storms / total_events
-        quants.append(quant)
-    return quants
-
-quants = return_period_to_quantile(ds_sst_compound, sst_recurrence_intervals)
-
-
-#%% compute quantiles
-ds_quants = ds_sst_compound.quantile(quants, dim = ["storm_id", "realization", "year"], method="closest_observation")
-ds_quants = ds_quants.assign_coords(dict(quantile = sst_recurrence_intervals))
-ds_quants = ds_quants.rename((dict(quantile="flood_return_yrs")))
-
-ds_quants["node_trns_flooding_cubic_meters"] = np.log10(ds_quants["node_flooding_cubic_meters"]+.01)
-
-#%% plotting return periods for each flood volume
-df_quants = ds_quants.to_dataframe()
-df_quants = df_quants.reset_index()
-#%%
-gdf_node_flding = gdf_nodes.merge(df_quants, how = 'inner', left_on = "NAME", right_on = "node_id").drop("NAME", axis=1)
 #%% computing bootstrap confidence intervals around flood volumes
 lst_ds_bs_CIs = []
 for node in tqdm(np.unique(ds_sst_compound.node_id.values)):
@@ -103,7 +33,7 @@ for node in tqdm(np.unique(ds_sst_compound.node_id.values)):
         for flood_return_yrs in ds_bootstrap_rtrn.flood_return_yrs.values:
             count += 1
             quant = quants[count]
-            node_flooding = df_quants[(df_quants.node_id==node) & (df_quants.flood_return_yrs == flood_return_yrs)].node_flooding_cubic_meters.values
+            node_flooding = df_quants_fld[(df_quants_fld.node_id==node) & (df_quants_fld.flood_return_yrs == flood_return_yrs)].node_flooding_cubic_meters.values
 
             lower_quant =  (1-sst_conf_interval) / 2
             upper_quant = 1 - lower_quant
@@ -128,9 +58,12 @@ df_bs_CIs = pd.read_csv(scratch_file.format("df_bootstrapped_CIs.csv"))
 df_bs_CIs.groupby("node_id")
 
 lst_ds_comp = []
+lst_df_node_fld_rtrn_event_id = []
 for node in tqdm(df_bs_CIs.node_id.unique()):
     count = -1
+    # break
     for flood_return_yrs in ds_bootstrap_rtrn.flood_return_yrs.values:
+        # break
         count += 1
         quant = quants[count]
         # node_flooding = df_quants[(df_quants.node_id==node) & (df_quants.flood_return_yrs == flood_return_yrs)].node_flooding_cubic_meters.values
@@ -149,7 +82,10 @@ for node in tqdm(df_bs_CIs.node_id.unique()):
         lower_bound = float(df_bs_CIs_subset["lower_CI"].values[0])
         lower_cond = ((df_sst_compound_subset.node_flooding_cubic_meters.values >= lower_bound) & (df_sst_compound_subset.node_flooding_cubic_meters.values > 0)) # don't want to include events with no flooding
 
-        df_closest_events_filtered = df_sst_compound_subset[(upper_cond) & (lower_cond)]
+        df_closest_events_filtered = df_sst_compound_subset[(upper_cond) & (lower_cond)].copy()
+        df_closest_events_filtered["node_id"] = node
+        df_closest_events_filtered["flood_return_yrs"] = flood_return_yrs
+        lst_df_node_fld_rtrn_event_id.append(df_closest_events_filtered)
 
         df_compare = df_closest_events_filtered.merge(ds_sst_freebndry_subset, on = ["storm_id", "realization", "year"], suffixes = ("_cmpnd", "_free"))
 
@@ -176,3 +112,7 @@ for node in tqdm(df_bs_CIs.node_id.unique()):
 
 df_comparison = pd.concat(lst_ds_comp)
 df_comparison.to_csv(f_bootstrapping_analysis, index=True)
+
+
+df_return_pd_analysis = pd.concat(lst_df_node_fld_rtrn_event_id, ignore_index = True)
+df_return_pd_analysis.to_csv(f_return_pd_analysis, index=False)
