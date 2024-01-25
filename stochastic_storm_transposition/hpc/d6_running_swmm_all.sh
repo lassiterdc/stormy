470c9c1518d32f1f78da7e821dfd54893e0c241e
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-01-25 18:47:45 -0500

    changing test case

--- stochastic_storm_transposition/hpc/d6_running_swmm_all.sh
+++ stochastic_storm_transposition/hpc/d6_running_swmm_all.sh
@@ -26,4 +26,4 @@ source activate running_swmm
 
 # running swmm
 python ${assar_dirs[hpc_d6_py]} ${SLURM_ARRAY_TASK_ID} "all" 1 1 # arguments: year, which models to run (failed, all, or specific storm number), which realizations to run, and whether to delete swmm .out files
-# python ${assar_dirs[hpc_d6_py]} 764 "all" "all" 0
+# python ${assar_dirs[hpc_d6_py]} 764 "all" 2 0
