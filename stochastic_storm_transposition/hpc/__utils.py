d5c6f9699bb356a7a1ca693ea7b9b84497fbd1f8
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-01-29 14:11:02 -0500

    converting a sys.exit statement to a warning statement

--- stochastic_storm_transposition/hpc/__utils.py
+++ stochastic_storm_transposition/hpc/__utils.py
@@ -258,7 +258,7 @@ def return_flood_losses_and_continuity_errors(swmm_rpt):
         if encountered_end_of_node_flooding_summary == False:
             lst_node_fld_summary.append(line)
     if encountered_flow_routing_continuity == False:
-        sys.exit("RPT file does not contain necessary fields")
+        print("WARNING: RPT file does not contain necessary fields")
     # the rpt file only has nodes with nonzero flooding but I need to account for all nodes
     # create pandas series of node flood summaries
     # return ids of all nodes
