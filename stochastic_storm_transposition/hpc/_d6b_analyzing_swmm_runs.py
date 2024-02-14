8d66daf802b73c09da37126344f3ae080c0fc70a
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-02-14 16:38:45 -0500

    documenting runs not attempted due to timeout

--- stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
+++ stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
@@ -23,20 +23,44 @@ df_perf = pd.concat(lst_dfs_perf)
 df_perf = df_perf.set_index(["swmm_inp"], drop=True)
 
 # update the consolidated performance summary with the re-runs
-lst_dfs_perf_reruns = []
-for f in lst_f_perf_reruns:
-    df_reruns = pd.read_csv(f)
-    lst_dfs_perf_reruns.append(df_reruns)
-df_perf_reruns = pd.concat(lst_dfs_perf_reruns)
-df_perf_reruns = df_perf_reruns.set_index(["swmm_inp"], drop=True)
+if len(lst_f_perf_reruns) > 0:
+    lst_dfs_perf_reruns = []
+    for f in lst_f_perf_reruns:
+        df_reruns = pd.read_csv(f)
+        lst_dfs_perf_reruns.append(df_reruns)
+    df_perf_reruns = pd.concat(lst_dfs_perf_reruns)
+    df_perf_reruns = df_perf_reruns.set_index(["swmm_inp"], drop=True)
+    for ind, row in df_perf_reruns.iterrows():
+        df_perf.loc[ind, :] = row
 
-for ind, row in df_perf_reruns.iterrows():
-    df_perf.loc[ind, :] = row
+# identify all runs that weren't attempted
+## load all swmm scenarios
+lst_f_swmm_scenarios = glob(f_swmm_scenarios_catalog.format("*"))
+lst_dfs_strms = []
+for f in lst_f_swmm_scenarios:
+    df = pd.read_csv(f)
+    lst_dfs_strms.append(df)
+df_strms = pd.concat(lst_dfs_strms)
+df_strms = df_strms.sort_values(["realization", "year", "storm_id"])
+df_strms.drop(columns = "simulation_index", inplace = True)
+df_strms.reset_index(drop=True, inplace=True)
+df_strms['dummy'] = 0
+idx_vals = ["swmm_inp", 'realization', 'year', 'storm_id', 'rainfall_0', 'rainfall_1',
+       'rainfall_2', 'rainfall_3', 'rainfall_4', 'rainfall_5', 'water_level']
+df_strms.set_index(idx_vals, inplace = True)
+
+df_perf = df_perf.reset_index()
+df_perf = df_perf.set_index(idx_vals, drop=True)
+df_perf['run_attempted'] = 1
+df_perf = df_perf.join(df_strms['dummy'], how = "right")
+df_perf['run_attempted'] = df_perf['run_attempted'].fillna(0).astype(bool)
+df_perf.drop(columns="dummy", inplace = True)
+df_perf = df_perf.reset_index()
 
 
 # export performance dataframe
-df_perf.to_csv(f_model_perf_summary, index=True)
-
+df_perf.to_csv(f_model_perf_summary, index=False)
+print("Exported file {}".format(f_model_perf_summary))
 
 #%% load event summaries
 lst_f_events = glob(f_summaries)
@@ -53,3 +77,4 @@ df_events = pd.concat(lst_dfs_events)
 
 # export eventsormance dataframe
 df_events.to_csv(f_events_summary, index=False)
+print("Exported file {}".format(f_events_summary))
