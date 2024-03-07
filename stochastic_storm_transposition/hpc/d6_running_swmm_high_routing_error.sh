d82b8689cf61e82b146f5d51cc721b58b114de87
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-03-07 14:05:31 -0500

    updating to run all jobs

--- stochastic_storm_transposition/hpc/d6_running_swmm_high_routing_error.sh
+++ stochastic_storm_transposition/hpc/d6_running_swmm_high_routing_error.sh
@@ -5,7 +5,7 @@
 #SBATCH -p standard				# Queue name "standard" (serial)
 #SBATCH -A quinnlab				# allocation name # can use dev for testing
 #SBATCH -t 32:00:00				# Run time per serial job (hh:mm:ss)
-#SBATCH --array=164-1000 # Array of jobs, 1 for each of 1000 years (1-1000)
+#SBATCH --array=1-1000 # Array of jobs, 1 for each of 1000 years (1-1000)
 #SBATCH --mem-per-cpu=16000
 #SBATCH --mail-user=dcl3nd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL   
