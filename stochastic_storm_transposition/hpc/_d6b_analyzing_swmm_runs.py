f3c333b2ed28e2a23b94da37c655fb623f80ad29
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-03-08 13:44:47 -0500

    adding function to consider sim. end time when deciding which outputs to include

--- stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
+++ stochastic_storm_transposition/hpc/_d6b_analyzing_swmm_runs.py
@@ -3,13 +3,16 @@ import pandas as pd
 from __utils import *
 from glob import glob
 from pathlib import Path
-
+from datetime import datetime
 
 f_performance = dir_swmm_sst_models + "_model_performance_year{}.csv".format("*")
 # f_summaries = dir_time_series + "_event_summary_year{}.csv".format("*")
 f_summaries = f_swmm_scenarios_catalog.format("*")
 f_performance_reruns = dir_swmm_sst_models + "_model_performance_year{}_failed_run_id{}.csv".format("*", "*")
 f_performance_high_error = dir_swmm_sst_models + "_model_performance_year{}_high_error.csv".format("*")
+# define file pattern of rpt files
+
+
 #%% load performance dataframe
 lst_f_perf = glob(f_performance)
 lst_f_perf_reruns = glob(f_performance_reruns)
@@ -19,6 +22,7 @@ for f in lst_f_perf:
     if ("failed_run" in f) or ("high_error" in f): # skip csvs from failed runs and re-runs with high errors
         continue
     df = pd.read_csv(f)
+    df["run_type"] = "initial"
     lst_dfs_perf.append(df)
 df_perf = pd.concat(lst_dfs_perf)
 df_perf = df_perf.set_index(["swmm_inp"], drop=True)
@@ -28,6 +32,7 @@ if len(lst_f_perf_reruns) > 0:
     lst_dfs_perf_reruns = []
     for f in lst_f_perf_reruns:
         df_reruns = pd.read_csv(f)
+        df_reruns["run_type"] = "rerun_of_failed_sims"
         lst_dfs_perf_reruns.append(df_reruns)
     df_perf_reruns = pd.concat(lst_dfs_perf_reruns)
     df_perf_reruns = df_perf_reruns.set_index(["swmm_inp"], drop=True)
@@ -39,11 +44,23 @@ if len(lst_f_perf_high_error_reruns) > 0:
     lst_dfs_perf_high_error_reruns = []
     for f in lst_f_perf_high_error_reruns:
         df_high_error_reruns = pd.read_csv(f)
+        df_high_error_reruns["run_type"] = "rerun_of_sims_with_high_routing_error"
         lst_dfs_perf_high_error_reruns.append(df_high_error_reruns)
     df_perf_high_error_reruns = pd.concat(lst_dfs_perf_high_error_reruns)
     df_perf_high_error_reruns = df_perf_high_error_reruns.set_index(["swmm_inp"], drop=True)
+    # check and see if there is a failed re-run and use the most recent result
     for ind, row in df_perf_high_error_reruns.iterrows():
-        df_perf.loc[ind, :] = row
+        # check if this high_error re-run is present in the failed runs too
+        use_high_error_result = True
+        if ind in df_perf_reruns.index:
+            row_failed_rerun = df_perf_reruns.loc[ind,:]
+            analysis_end_datetime_failed = return_analysis_end_date(ind, row_failed_rerun.routing_timestep)
+            analysis_end_datetime_higherror_rerun = return_analysis_end_date(ind, row.routing_timestep)
+            # if the failed re-run has an analysis end date LATER than the high error run, keep the failed re-run
+            if analysis_end_datetime_failed > analysis_end_datetime_higherror_rerun:
+                use_high_error_result = False
+        if use_high_error_result:
+            df_perf.loc[ind, :] = row
 
 # identify all runs that weren't attempted
 ## load all swmm scenarios
