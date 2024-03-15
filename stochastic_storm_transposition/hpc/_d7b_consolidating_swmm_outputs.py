c9f6283a598792dcd38222777eaf6b3a6207e500
Daniel Lassiter <64621312+lassiterdc@users.noreply.github.com>
2024-03-15 13:44:50 -0400

    removing function to incorporate previous run attempts

--- stochastic_storm_transposition/hpc/_d7b_consolidating_swmm_outputs.py
+++ stochastic_storm_transposition/hpc/_d7b_consolidating_swmm_outputs.py
@@ -9,37 +9,22 @@ from __utils import *
 script_start_time = datetime.now()
 
 f_modelresults = dir_swmm_sst_models + "_model_outputs_year{}.nc".format("*")
-f_modelresults_rerun = dir_swmm_sst_models + "_model_outputs_year{}_failed_run_id{}.nc".format("*", "*")
+f_modelresults_failed_rerun = dir_swmm_sst_models + "_model_outputs_year{}_failed_run_id{}.nc".format("*", "*")
+f_modelresults_high_error_rerun = dir_swmm_sst_models + "_model_outputs_year{}_high_error.nc".format("*")
+
+df_perf = pd.read_csv(f_model_perf_summary)
 
 lst_f_modresults = []
 for f in glob(f_modelresults):
-    if "failed_run" not in f:
+    if ("failed_run" not in f) and ("high_error" not in f):
         lst_f_modresults.append(f)
-lst_f_modresults_rerun = glob(f_modelresults_rerun)
-#%% export to single netcdf file
-# def open_dataset_and_assign_attrs(lst_files):
-#     ds = xr.open_mfdataset(lst_files, engine="h5netcdf")
-#     attrs = dict(date_created = str(datetime.now()))
-#     ds.attrs = attrs
-#     return ds
+lst_f_modresults_failed_rerun = glob(f_modelresults_failed_rerun)
+lst_f_modresults_high_error_rerun = glob(f_modelresults_high_error_rerun)
 
-# DCL WORK
-# ds_first = xr.open_dataset(lst_f_modresults[0])
-# for f in lst_f_modresults[1:]:
-#     ds_next = xr.open_dataset(f)
-#     # attempt to combine datasets
-#     try:
-#         test_ds = xr.combine_by_coords([ds_first, ds_next])
-#     except Exception as e:
-#         print("Combining datasets failed due to error:")
-#         print(e)
-#         print("####")
-#         print("Problem dataset: {}".format(f))
-#         print(ds_next)
-#         print("###################################################")
+if len(lst_f_modresults_failed_rerun + lst_f_modresults_high_error_rerun):
+    sys.exit("There are model re-runs that need to be incorporated in the results. Run d6_running_previous.sh script before proceeding.")
 
-
-    # xr.combine_nested([ds_first, ds_next])
+#%% define functions
 def open_dataset_and_assign_attrs(lst_files):
     lst_ds = []
     for f in lst_files:
@@ -48,22 +33,51 @@ def open_dataset_and_assign_attrs(lst_files):
     attrs = dict(date_created = str(datetime.now()))
     ds_combined.attrs = attrs
     return ds_combined
-# END DCL WORK
 
+def update_results_dataset_with_reurns(lst_f_modresults_rerun, ds_results):
+    """
+    On the need for using data arrays to subset an xarray dataset at a set of specific coordinates
+    rather than slicing along the dimensions:
+    https://docs.xarray.dev/en/stable/user-guide/indexing.html#more-advanced-indexing
+    """
+    # do nothing if there are no files
+    if len(lst_f_modresults_rerun) == 0:
+        return
+    # load dataset of reruns
+    ds_results_rerun_error = open_dataset_and_assign_attrs(lst_f_modresults_rerun)
+    # create series of re-run simulation end times
+    s_endtimes_ds_rerun = ds_results_rerun_error.analysis_end_datetime.to_dataframe().dropna().analysis_end_datetime
+    rerun_pd_indices = s_endtimes_ds_rerun.index
+    # subset analysis endtimes from ds_results
+    s_endtimes_ds_results = ds_results.analysis_end_datetime.to_dataframe().analysis_end_datetime[pd.IndexSlice[rerun_pd_indices]]
+    # determine where rerun analysis endtime is more recent than those in ds_results
+    ind_of_ds_results_to_update = s_endtimes_ds_rerun[s_endtimes_ds_rerun > s_endtimes_ds_results].index
+    # only update ds_results where the rerun result has a more recent simulation endtime
+    if len(ind_of_ds_results_to_update) > 0:
+        df_rerun_indices = ind_of_ds_results_to_update.to_frame(index = False)
+        da_rerun_indices = df_rerun_indices.to_xarray()
+        # create a selection dictionary to subset the ds_results dataset to ONLY the sims that were re-run
+        dict_rerun_idx = dict(realization = da_rerun_indices["realization"], storm_id = da_rerun_indices["storm_id"],
+                        year = da_rerun_indices["year"], sim_type = da_rerun_indices["sim_type"])
+        # update the ds_results dataframe with the re-runs
+        ds_results.loc[dict_rerun_idx] = ds_results_rerun_error.sel(dict_rerun_idx)
+
+#%% load results and update simulation results that were re-run more recently
+bm_time = datetime.now()
 ds_results = open_dataset_and_assign_attrs(lst_f_modresults)
+tot_elapsed_time_min = round((datetime.now() - bm_time).seconds / 60, 1)
+print("Minutes to open results dataset: {}".format(tot_elapsed_time_min))
 
+# bm_time = datetime.now()
+# update_results_dataset_with_reurns(lst_f_modresults_failed_rerun, ds_results)
+# tot_elapsed_time_min = round((datetime.now() - bm_time).seconds / 60, 1)
+# print("Minutes to update results with failed model re-runs: {}".format(tot_elapsed_time_min))
 
-# update ds_results where the model has been re-run
-if len(lst_f_modresults_rerun) > 0:
-    for f in lst_f_modresults_rerun:
-        ds_results_rerun = open_dataset_and_assign_attrs(f)
-        # ds_results_rerun_loaded = ds_results_rerun.load()
-        dict_rerun_idx = dict()
-        for coord in ds_results_rerun.coords:
-            dict_rerun_idx[coord] = ds_results_rerun[coord].values
-    # https://docs.xarray.dev/en/stable/user-guide/indexing.html#assigning-values-with-indexing
-    ds_results.loc[dict_rerun_idx] = ds_results_rerun.sel(dict_rerun_idx)
-
+# bm_time = datetime.now()
+# update_results_dataset_with_reurns(lst_f_modresults_high_error_rerun, ds_results)
+# tot_elapsed_time_min = round((datetime.now() - bm_time).seconds / 60, 1)
+# print("Minutes to update results with high-error model re-runs: {}".format(tot_elapsed_time_min))
+#%% export results to a netcdf file
 ds_results_loaded = ds_results.load() # this seems to speed up the writing of the netcdf file
 ds_results_loaded.to_netcdf(f_model_outputs_consolidated, encoding= {"node_flooding_cubic_meters":{"zlib":True}}, engine = "h5netcdf")
 
